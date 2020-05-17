e6720ac55fc2a897aea3d4f326d4e972968c50ef
Nikas Klimciauskas <klimciauskasnikas@gmail.com>
2020-05-17 23:29:16 +0300

    Install doctrine fixtures

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
