41b48df152485a7fcaef010f779a028595fbaad6
Nikas Klimciauskas <klimciauskasnikas@gmail.com>
2020-05-19 16:15:23 +0300

    voter logic to allow only owner of the phone book entry to see it

--- src/src/Security/Voter/PhoneBookVoter.php
+++ src/src/Security/Voter/PhoneBookVoter.php
@@ -2,6 +2,7 @@
 
 namespace App\Security\Voter;
 
+use App\Entity\PhoneBook;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -10,15 +11,18 @@ class PhoneBookVoter extends Voter
 {
     protected function supports($attribute, $subject)
     {
-        // replace with your own logic
-        // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, ['POST_EDIT', 'POST_VIEW'])
-            && $subject instanceof \App\Entity\BlogPost;
+        // only vote on `PhoneBook` objects
+        if (!$subject instanceof PhoneBook) {
+            return false;
+        }
+
+        return true;
     }
 
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
         $user = $token->getUser();
+
         // if the user is anonymous, do not grant access
         if (!$user instanceof UserInterface) {
             return false;
@@ -26,13 +30,12 @@ class PhoneBookVoter extends Voter
 
         // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
-            case 'POST_EDIT':
-                // logic to determine if the user can EDIT
-                // return true or false
-                break;
-            case 'POST_VIEW':
-                // logic to determine if the user can VIEW
-                // return true or false
+            case 'CAN_EDIT':
+                foreach($user->getPhoneBook() as $item){
+                    if($item->getId() == $subject->getId()){
+                        return true;
+                    }
+                }
                 break;
         }
 
